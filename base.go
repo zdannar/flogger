2a826a943dc2c42c7b162793d7c1e80f92a9e456
Zack Dannar <zack.dannar@rightnow.com>
2013-12-12 10:43:56 -0700

    Building repo.

---
+++ base.go
@@ -0,0 +1,120 @@
+package flogger
+
+import (
+    "log"
+    "os"
+)
+
+const (
+    DEBUG int = iota
+    INFO
+    WARNING
+    ERROR
+    FATAL
+    PANIC
+)
+
+const (
+    STDOUT int = iota
+    STDERR int = iota
+)
+
+var (
+    FLOG_APPEND int = os.O_WRONLY|os.O_APPEND|os.O_CREATE
+    FLOG_FORMAT int = log.LstdFlags|log.Lshortfile
+
+    FLOG_LEVELS = map[int] string {
+        DEBUG : "DEBUG",
+        INFO : "INFO",
+        WARNING : "WARNING",
+        ERROR : "ERROR",
+        FATAL : "FATAL",
+        PANIC : "PANIC",
+    }
+    defLogr *Flogger = New(DEBUG, FLOG_FORMAT, FLOG_LEVELS)
+)
+
+// Closes default logger's file descriptor.
+func Close() error {
+    return defLogr.Close()
+}
+
+// Opens default logger's file for writing. 
+func OpenFile(logPath string, openMode int, perms os.FileMode) (error) {
+    return defLogr.OpenFile(logPath, openMode, perms)
+}
+
+// Sets the default logger's logging level.
+func SetLevel(level int) {
+    defLogr.SetLevel(level)
+}
+
+// Sets the default logger's level map.
+func SetLevelMap(lmap map[int]string) {
+    defLogr.SetLevelMap(lmap)
+}
+
+// Calls f.Debug() on base logging object.
+func Debug(args ...interface{}) {
+   defLogr.Debug(args...)
+}
+
+// Calls f.Info() on base logging object.
+func Info(args ...interface{}) {
+   defLogr.Info(args...)
+}
+
+// Calls f.Warning() on base logging object.
+func Warning(args ...interface{}) {
+   defLogr.Warning(args...)
+}
+
+// Calls f.Error() on base logging object.
+func Error(args ...interface{}) {
+   defLogr.Error(args...)
+}
+
+// Calls f.Fatal() on base logging object.
+func Fatal(args ...interface{}) {
+   defLogr.Fatal(args...)
+}
+
+// Calls f.Panic() on base logging object.
+func Panic(args ...interface{}) {
+   defLogr.Panic(args...)
+}
+
+// Calls f.RedirectStreams() on base logging object.
+func RedirectStreams() {
+   defLogr.RedirectStreams()
+}
+
+// Calls f.Debugf() on base logging object.
+func Debugf(msg string, args ...interface{}) {
+   defLogr.Debugf(msg, args...)
+}
+
+// Calls f.Infof() on base logging object.
+func Infof(msg string, args ...interface{}) {
+   defLogr.Infof(msg, args...)
+}
+
+// Calls f.Warningf() on base logging object.
+func Warningf(msg string, args ...interface{}) {
+   defLogr.Warningf(msg, args...)
+}
+
+// Calls f.Errorf() on base logging object.
+func Errorf(msg string, args ...interface{}) {
+   defLogr.Errorf(msg, args...)
+}
+
+// Calls f.Fatalf() on base logging object.
+func Fatalf(msg string, args ...interface{}) {
+   defLogr.Fatalf(msg, args...)
+}
+
+// Calls f.Panicf() on base logging object.
+func Panicf(msg string, args ...interface{}) {
+   defLogr.Panicf(msg, args...)
+}
---
+++ flogger.go
@@ -0,0 +1,176 @@
+/*
+Flogger provides a simple wrapper around go's base logging library to provide
+logging levels.  Further documentation can be found at:
+somewhere.... 
+*/
+package flogger
+
+import (
+    "log"
+    "os"
+    "fmt"
+    "syscall"
+)
+
+// Returns a flogger.Flogger structure for logging.
+func New(logLevel, logFormat int, levelMap map[int]string) *Flogger {
+    return &Flogger{ fd: os.Stdout,
+                     level: logLevel,
+                     logForm: logFormat,
+                     log : log.New(os.Stderr, "", FLOG_FORMAT),
+                     lvlMap: levelMap }
+}
+
+// Flogger structure.
+type Flogger struct {
+    lpath string
+    fd *os.File
+    log *log.Logger
+    level int
+    logForm int
+    lvlMap map[int]string
+}
+
+// Opens a file for flogger to log to.  
+func (f *Flogger) OpenFile(logPath string, openMode int, perms os.FileMode) (error) {
+
+    f.lpath = logPath
+    fd, err := os.OpenFile(f.lpath, openMode, perms)
+    if err != nil {
+        return err
+    }
+    f.log, f.fd = log.New(fd, "", f.logForm), fd
+    return nil
+}
+
+// Flushes and closes file descriptor that flogger is logging to.  Should always be 
+// called at the end of execution.
+func (f *Flogger) Close() error {
+    if err := f.fd.Sync(); err != nil {
+        return err
+    }
+    return f.fd.Close()
+}
+
+// Sets the logger level. Usually called with flogger.INFO, flogger.DEBUG, etc...
+func (f *Flogger) SetLevel(level int) {
+    f.level = level
+}
+
+// Sets logging level map. Provides the abilty to change level messages.
+// Default logger uses flogger.FLOG_LEVELS
+func (f *Flogger) SetLevelMap(lmap map[int]string) {
+    //TODO: Add level checking
+    f.lvlMap = lmap
+}
+
+// Provides the mapping for text of the log levels
+func (f *Flogger) appLevel(level int, msg string) string {
+    return fmt.Sprintf(": %s : ", f.lvlMap[level]) + msg
+}
+
+// Base method that actually applies the levels and wraps log.Print, 
+// log.Fatal, log.Panic
+func (f *Flogger) flog(level int, args ...interface{}) {
+    if level < f.level { return }
+    ml := []interface{}{f.appLevel(level, "")}
+    v := append(ml,args...)
+    s := fmt.Sprint(v...)
+    f.log.Output(4, s)
+    switch {
+        case level == FATAL:
+            os.Exit(1)
+        case level == PANIC:
+            panic(s)
+    }
+}
+
+// Base method that actually applies the levels and wraps log.Printf, 
+// log.Fatalf, log.Panicf
+func (f *Flogger) flogf(level int, msg string, args ...interface{}) {
+    if level < f.level { return }
+
+    ml := f.appLevel(level,msg)
+    f.log.Output(4, fmt.Sprintf(ml, args...))
+
+    switch {
+        case level == FATAL:
+            os.Exit(1)
+        case level == PANIC:
+            panic(msg)
+    }
+}
+
+// Redirects stdout and stderr to log file.  Very handy to catch the output of 
+// panics in your log file.
+func (f *Flogger) RedirectStreams() {
+    f.RedirectStream(STDOUT)
+    f.RedirectStream(STDERR)
+}
+
+// Redirects file descriptor to log file. 
+// Example: flogger.RedirectStream(flogger.STDERR)
+func (f *Flogger) RedirectStream(fd int) {
+    syscall.Dup2(int(f.fd.Fd()), fd)
+}
+
+// Logs to the debugging logging channel 
+func (f *Flogger) Debug(args ...interface{}) {
+   f.flog(DEBUG, args...)
+}
+
+// Logs to the info logging channel 
+func (f *Flogger) Info(args ...interface{}) {
+   f.flog(INFO, args...)
+}
+
+// Logs to the warning logging channel 
+func (f *Flogger) Warning(args ...interface{}) {
+   f.flog(WARNING, args...)
+}
+
+// Logs to the error logging channel 
+func (f *Flogger) Error(args ...interface{}) {
+   f.flog(ERROR, args...)
+}
+
+// Logs your message, then calls os.Exit(1).
+func (f *Flogger) Fatal(args ...interface{}) {
+   f.flog(FATAL, args...)
+}
+
+// Panics with your message.
+func (f *Flogger) Panic(args ...interface{}) {
+   f.flog(PANIC, args...)
+}
+
+
+// Debug function with string formatting
+func (f *Flogger) Debugf(msg string, args ...interface{}) {
+   f.flogf(DEBUG, msg, args...)
+}
+
+// Info function with string formatting
+func (f *Flogger) Infof(msg string, args ...interface{}) {
+   f.flogf(INFO, msg, args...)
+}
+
+// Warning function with string formatting
+func (f *Flogger) Warningf(msg string, args ...interface{}) {
+   f.flogf(WARNING, msg, args...)
+}
+
+// Error function with string formatting
+func (f *Flogger) Errorf(msg string, args ...interface{}) {
+   f.flogf(ERROR, msg, args...)
+}
+
+// Fatal function with string formatting
+func (f *Flogger) Fatalf(msg string, args ...interface{}) {
+   f.flogf(FATAL, msg, args...)
+}
+
+// Panic function with string formatting
+func (f *Flogger) Panicf(msg string, args ...interface{}) {
+   f.flogf(PANIC, msg, args...)
+}
